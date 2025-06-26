0ceb0c7488e33409e39d8790c90e677c1945290f
anan pengkhun <se56160422@gmail.com>
2025-06-26 17:30:53 +0700

    test

--- app/nav_bar_common.tsx
+++ app/nav_bar_common.tsx
@@ -2,6 +2,7 @@
 "use client";
 export default function NavBarCommon() {
   return (
+    
     <nav className="flex justify-center gap-4 p-4 border-b">
       <button className="text-blue-500 hover:underline" onClick={() => window.location.href = '/dashboard'}>
         Home
